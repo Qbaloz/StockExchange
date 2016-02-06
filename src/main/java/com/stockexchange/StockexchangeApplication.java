d350d374d7ff400b202f5637fe9a5e112798b857
Jakub Wawrzyniak <qbaloz1991@gmail.com>
2016-02-06 16:18:52 +0100

    first commit

---
+++ src/main/java/com/stockexchange/StockexchangeApplication.java
@@ -0,0 +1,12 @@
+package com.stockexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockexchangeApplication.class, args);
+	}
+}
